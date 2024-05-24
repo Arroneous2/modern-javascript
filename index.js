b58d620a21a25b0b511a6f196c64e58e50ce22d6
Arroneous2 <thumpinbassbeat@gmail.com>
2024-05-24 13:13:08 -0500

    test out babel with newer syntax

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ console.log("Why hallo thar");
 var datetime = moment().format();
 console.log(datetime);
 console.log(mathjs.sqrt(-4).toString());
+
+var [a, , b] = [1, 2, 3];
+a === 1;
+b === 3;
+console.log([a, b]);
